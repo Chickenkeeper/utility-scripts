d7608bfe14b69a9852621cebbded2997e47e9c35
Chris Moore <artofchrism@gmail.com>
2026-09-12 16:21:23 +0100

    Improve clamscan directory exclusions
    Anchor /sys to avoid over-matching, and also exclude the /proc and /dev virtual filesystems as well as btrfs snapshots.

--- clamav-full-scan.sh
+++ clamav-full-scan.sh
@@ -2,4 +2,4 @@
 
 # NOTE: this needs to be run as root, otherwise it will generate a lot of 'permission denied' errors
 
-freshclam && clamscan -ir --log="~/.clamlog" --exclude-dir="^/sys" /
+freshclam && clamscan -ir --log="~/.clamlog" --exclude-dir="^/dev/" --exclude-dir="^/proc/" --exclude-dir="^/sys/" --exclude-dir="/\.snapshots/.*/snapshot/" /
