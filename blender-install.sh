bf53e97b04d92a64f61e8454d3bb2cfd7bf3e6a4
Chris Moore <artofchrism@gmail.com>
2026-09-20 14:20:34 +0100

    Install Blender locally instead of globally
    Some users may not want Blender in their application launcher, or may want to pin their version of Blender during the lifetime of a project for consistency and to avoid any incompatibilities introduced by future versions. So, make the Blender installer install locally instead of globally so instances can be individually managed.

--- blender-install.sh
+++ blender-install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# NOTE: this needs to be run as root since it needs access
-# to /opt and the ability to register Blender system-wide
-
 if [ "$#" -ne 1 ]; then
     echo "error: missing source tar"
     echo "usage: ./blender-update.sh [SRC]"
@@ -15,16 +12,16 @@ if [ ! -f "$1" ]; then
     exit 1
 fi
 
-blender_dir=/opt/blender
+blender_dir=$HOME/.local/opt/blender
 
 # if the target directory exists then unregister blender and
 # delete everything in the directory, otherwise create the directory
 if [ -d "$blender_dir" ]; then
-    $blender_dir/blender --unregister-allusers
+    $blender_dir/blender --unregister
     rm -rf $blender_dir/*
 else
-    mkdir $blender_dir
+    mkdir -p $blender_dir
 fi
 
 tar -xf "$1" -C "$blender_dir" --strip-components 1
-$blender_dir/blender --register-allusers
+$blender_dir/blender --register
